77368aebae3470cefbfd8f4151409037bfe94875
patjet187 <120464474+patjet187@users.noreply.github.com>
2023-10-24 18:16:45 -0700

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,16 @@
+public class FavoriteSong {
+  public static void main (String[] args) {
+  
+  System.out.println("...Ready For It?");
+  System.out.println("Song by Taylor Swift\n");
+  
+  System.out.println("Chorus");
+  System.out.println("In the middle of the night, in my dreams");
+  System.out.println("You should see the things we do, baby (mmm)");
+  System.out.println("In the middle of the night, in my dreams");
+  System.out.println("I know I'm gonna be with you");
+  System.out.println("So I'll take my time");
+  System.out.println("Are you ready for it?");
+  
+  }
+}
